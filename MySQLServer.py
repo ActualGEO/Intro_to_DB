5f55f180e3acb089f8a4d660180f992ae211b4f2
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-12-30 14:53:59 +0100

    added mysqlserver file

---
+++ MySQLServer.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+
+try:
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password
+    )
+    print("Database connected successfully")
+
+except Error as e:
+    print(f"Connection Error '({e})'")
+
+
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_store_created' successfully!")
+
+    
+except mysql.connector.Error as e:
+    print(f"Databse error: '{e}'")
+
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+        print("Cursor closed")
+    
+    if connection.is_connected() and cursor:
+        connection.close()
+        print("Connection closed")
